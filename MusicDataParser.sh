0b00708400e0b50cad702b2bb943647ac34f274e
ducstream-code <aurelienkrief@gmail.com>
2022-02-16 11:13:10 +0100

    Creation db if not exists and duration

--- MusicDataParser.sh
+++ MusicDataParser.sh
@@ -5,8 +5,31 @@
 #TODO check if user is sudo if not exit
 
 #remove whitespaces in files in the folder
-rm data.txt
-rm musicList.sql
+FILE="data.txt"
+FILE2="musicList.sql"
+if test -f "$FILE"; then
+    rm $FILE
+fi
+
+if test -f "$FILE2"; then
+    rm $FILE2
+fi
+
+
+#database creation query if not exists
+echo "CREATE TABLE IF NOT EXISTS \`musics\` (
+  \`id\` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  \`title\` varchar(255) NOT NULL,
+  \`artist\` varchar(255) NOT NULL,
+  \`album\` varchar(255) NOT NULL,
+  \`date\` int(11) NOT NULL,
+  \`genre\` varchar(255) NOT NULL,
+  \`location\` varchar(255) NOT NULL,
+  \`image\` varchar(255) NOT NULL,
+  \`duration\` int(10) DEFAULT 0
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;" >> musicList.sql\
+
 find . -name "* *" -type f | rename 's/ /_/g'
 
 
@@ -29,20 +52,17 @@ ALBUM=$(cat data.txt | grep 'album=' | cut -d= -f2 | tr -d \" )
  
 DATE=$(cat data.txt | grep 'date=' |cut -d= -f2 | tr -d \"  )
 
-
-
 GENRE=$(cat data.txt | grep 'genre=' | cut -d= -f2 | tr -d \" ) 
 
 LOCATION=$(readlink -f $i | tr -d \"  )
 
-
-
 IMAGE=$(readlink -f $i | tr -d \"  )
 
+DURATION=$(mp3info -p "%S\n" $i | awk 'BEGIN { s = 0 }; { s = s + $1 }; END { print s }' )
 
-#echo $ID,$TITLE,$ARTIST,$ALBUM,$DATE,$GENRE,$LOCATION,$IMAGE
+#echo $ID,$TITLE,$ARTIST,$ALBUM,$DATE,$GENRE,$LOCATION,$IMAGE,$DURATION
 
-echo INSERT INTO \`musics\` \(title, artist, album, date, genre, location, image\) VALUES \(\"$TITLE\",\"$ARTIST\",\"$ALBUM\",$DATE,\"$GENRE\",\"$LOCATION\",\"$IMAGE\"\)\; >> musicList.sql
+echo INSERT INTO \`musics\` \(title, artist, album, date, genre, location, image, duration\) VALUES \(\"$TITLE\",\"$ARTIST\",\"$ALBUM\",$DATE,\"$GENRE\",\"$LOCATION\",\"$IMAGE\",$DURATION\)\; >> musicList.sql
 rm data.txt
 
 
